72946e67d7b8b517eb2ef28ae5402a9500dec9e2
Pavel Alimpiev <7024354+VaultVulp@users.noreply.github.com>
2024-05-06 21:39:29 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,38 +15,38 @@ if [ $EXTRACT_TAG_FROM_GIT_REF == "true" ]; then
   DOCKER_IMAGE_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
 fi
 
-DOCKER_IMAGE_NAME=$(echo ghcr.io/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME} | tr '[:upper:]' '[:lower:]')
+DOCKER_IMAGE_NAME=$(echo "ghcr.io/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME}" | tr '[:upper:]' '[:lower:]')
 DOCKER_IMAGE_NAME_WITH_TAG=$(echo "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" | tr '[:upper:]' '[:lower:]')
 
 docker buildx create --use # Creating builder instance to support cross-platform builds
 
 docker login -u publisher -p ${DOCKER_TOKEN} ghcr.io
 
-if [ $PULL_IMAGE == "true" ]; then
-  if [ $DOCKER_IMAGE_PLATFORM != "" ]; then
-    docker pull $DOCKER_IMAGE_NAME_WITH_TAG --platform $DOCKER_IMAGE_PLATFORM || docker pull $DOCKER_IMAGE_NAME --platform $DOCKER_IMAGE_PLATFORM || true
+if [ "$(echo "${PULL_IMAGE}")" == "true" ]; then
+  if [ "$(echo "${DOCKER_IMAGE_PLATFORM}")" != "" ]; then
+    docker pull "$(echo "${DOCKER_IMAGE_NAME_WITH_TAG}")" --platform "$(echo "${DOCKER_IMAGE_PLATFORM}")" || docker pull "$(echo "${DOCKER_IMAGE_NAME}")" --platform "$(echo "${DOCKER_IMAGE_PLATFORM}")" || true
   else
-    docker pull $DOCKER_IMAGE_NAME_WITH_TAG || docker pull $DOCKER_IMAGE_NAME || true
+    docker pull $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}")" || docker pull $(echo "${DOCKER_IMAGE_NAME}")" || true
   fi
 fi
 
 set -- -t $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}")
 
-if [ $DOCKERFILE != "Dockerfile" ]; then
-  set -- "$@" -f $DOCKERFILE
+if [ "$(echo "${DOCKERFILE}")" != "Dockerfile" ]; then
+  set -- "$@" -f "$(echo "${DOCKERFILE}")"
 fi
 
 if [ "$(echo "${DOCKER_IMAGE_PLATFORM}")" != "" ]; then
-  set -- "$@" --platform $DOCKER_IMAGE_PLATFORM
+  set -- "$@" --platform "$(echo "${DOCKER_IMAGE_PLATFORM}")"
 fi
 
 if [ "$(echo "${CUSTOM_DOCKER_BUILD_ARGS}")" != "" ]; then
-  set -- "$@" "$CUSTOM_DOCKER_BUILD_ARGS"
+  set -- "$@" "$(echo "${CUSTOM_DOCKER_BUILD_ARGS}")"
 fi
 
-set -- "$@" $BUILD_CONTEXT
+set -- "$@" "$(echo "${BUILD_CONTEXT}")"
 
-for tag in $DOCKER_IMAGE_TAGS
+for tag in "$(echo "${DOCKER_IMAGE_TAGS}")"
 do
     DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${tag} | tr '[:upper:]' '[:lower:]')
     set -- -t $DOCKER_IMAGE_NAME_WITH_TAG "$@"
