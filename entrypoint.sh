b797a6f3a463c562a17f633f73dc02c4c2a0e001
Pavel Alimpiev <7024354+VaultVulp@users.noreply.github.com>
2024-05-07 05:35:29 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ if [ $(echo ${DOCKER_IMAGE_PLATFORM}) != "" ]; then
 fi
 
 echo "Check args"
-if [ $(echo ${CUSTOM_DOCKER_BUILD_ARGS}) != "" ]; then
+if [ $(("${CUSTOM_DOCKER_BUILD_ARGS}")) != "" ]; then
   echo "Set args"
   set -- ${@} ${CUSTOM_DOCKER_BUILD_ARGS}
 fi
