80380a5a1d60089cf5a65f707e8ab472f7d45a4c
Pavel Alimpiev <7024354+VaultVulp@users.noreply.github.com>
2024-05-06 21:58:22 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,43 +18,38 @@ fi
 DOCKER_IMAGE_NAME=$(echo "ghcr.io/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME}" | tr '[:upper:]' '[:lower:]')
 DOCKER_IMAGE_NAME_WITH_TAG=$(echo "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" | tr '[:upper:]' '[:lower:]')
 
-a=$(echo "${DOCKER_IMAGE_PLATFORM}")
-a=$(echo "${DOCKER_IMAGE_PLATFORM}")
-a=$(echo "${DOCKER_IMAGE_PLATFORM}")
-
-
 docker buildx create --use # Creating builder instance to support cross-platform builds
 
 docker login -u publisher -p ${DOCKER_TOKEN} ghcr.io
 
-if [ $(echo "${PULL_IMAGE}") == "true" ]; then
-  if [ $(echo "${DOCKER_IMAGE_PLATFORM}") != "" ]; then
-    docker pull $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}") --platform $(echo "${DOCKER_IMAGE_PLATFORM}") || docker pull $(echo "${DOCKER_IMAGE_NAME}") --platform $(echo "${DOCKER_IMAGE_PLATFORM}") || true
-  else
-    docker pull $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}") || docker pull $(echo "${DOCKER_IMAGE_NAME}") || true
-  fi
-fi
+# if [ $(echo "${PULL_IMAGE}") == "true" ]; then
+#   if [ $(echo "${DOCKER_IMAGE_PLATFORM}") != "" ]; then
+#     docker pull $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}") --platform $(echo "${DOCKER_IMAGE_PLATFORM}") || docker pull $(echo "${DOCKER_IMAGE_NAME}") --platform $(echo "${DOCKER_IMAGE_PLATFORM}") || true
+#   else
+#     docker pull $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}") || docker pull $(echo "${DOCKER_IMAGE_NAME}") || true
+#   fi
+# fi
 
 set -- -t $(echo "${DOCKER_IMAGE_NAME_WITH_TAG}")
 
-if [ $(echo "${DOCKERFILE}") != "Dockerfile" ]; then
-  set -- $(echo "${@}") -f $(echo "${DOCKERFILE}")
-fi
+# if [ $(echo "${DOCKERFILE}") != "Dockerfile" ]; then
+#   set -- $(echo "${@}") -f $(echo "${DOCKERFILE}")
+# fi
 
-if [ $(echo "${DOCKER_IMAGE_PLATFORM}") != "" ]; then
-  set -- $(echo "${@}") --platform $(echo "${DOCKER_IMAGE_PLATFORM}")
-fi
+# if [ $(echo "${DOCKER_IMAGE_PLATFORM}") != "" ]; then
+#   set -- $(echo "${@}") --platform $(echo "${DOCKER_IMAGE_PLATFORM}")
+# fi
 
-if [ $(echo "${CUSTOM_DOCKER_BUILD_ARGS}") != "" ]; then
-  set -- $(echo "${@}") $(echo "${CUSTOM_DOCKER_BUILD_ARGS}")
-fi
+# if [ $(echo "${CUSTOM_DOCKER_BUILD_ARGS}") != "" ]; then
+#   set -- $(echo "${@}") $(echo "${CUSTOM_DOCKER_BUILD_ARGS}")
+# fi
 
-set -- $(echo "${@}") $(echo "${BUILD_CONTEXT}")
+# set -- $(echo "${@}") $(echo "${BUILD_CONTEXT}")
 
-for tag in $(echo "${DOCKER_IMAGE_TAGS}")
-do
-    DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${tag} | tr '[:upper:]' '[:lower:]')
-    set -- -t $DOCKER_IMAGE_NAME_WITH_TAG $(echo "${@}")
-done
+# for tag in $(echo "${DOCKER_IMAGE_TAGS}")
+# do
+#     DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${tag} | tr '[:upper:]' '[:lower:]')
+#     set -- -t $DOCKER_IMAGE_NAME_WITH_TAG $(echo "${@}")
+# done
 
 docker buildx build --push $(echo "${@}")
